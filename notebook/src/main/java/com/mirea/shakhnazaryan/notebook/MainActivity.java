8da1bd4bcbd93e3cf9bdc92e8f8c5d2a9bcc6b5b
adder <temartem68@mail.ru>
2022-05-14 19:03:52 +0300

    practice6

---
+++ notebook/src/main/java/com/mirea/shakhnazaryan/notebook/MainActivity.java
@@ -0,0 +1,82 @@
+package com.mirea.shakhnazaryan.notebook;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText text;
+    private EditText fileNameText;
+    private final static String FILE_NAME = "file_name";
+    private SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.editText1);
+        fileNameText = findViewById(R.id.editText2);
+        preferences = getPreferences(MODE_PRIVATE);
+    }
+
+    public void saveText(View view){
+        FileOutputStream fos = null;
+        try {
+            EditText textBox = findViewById(R.id.editText1);
+            String text = textBox.getText().toString();
+
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show();
+        }
+        catch(IOException ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        finally{
+            try{
+                if(fos!=null)
+                    fos.close();
+            }
+            catch(IOException ex){
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(FILE_NAME, fileNameText.getText().toString());
+        editor.apply();
+    }
+
+    public void openText(View view){
+        FileInputStream fin = null;
+        try {
+            fin = openFileInput(FILE_NAME);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String (bytes);
+            fileNameText.setText(text);
+        }
+        catch(IOException ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        finally{
+
+            try{
+                if(fin!=null)
+                    fin.close();
+            }
+            catch(IOException ex){
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
